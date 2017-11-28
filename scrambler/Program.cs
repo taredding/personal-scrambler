f55a717820a4912e18781b5ba8fbd0093efa9908
Trevor Alexander Redding <trevorredding5@gmail.com>
2017-11-28 12:58:43 -0500

    Added initial required files for the project.
    Nothing works yet, most methods are stubs.

---
+++ scrambler/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace scrambler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 3) {
+                Console.WriteLine("usage: dotnet run [task] [filename] [key]\n task:\n -s to scramble a file\n -u to unscramble");
+                return;
+            }
+            
+            String input = System.IO.File.ReadAllText(args[0]);
+            Console.WriteLine(input);
+
+        }
+    }
+}
