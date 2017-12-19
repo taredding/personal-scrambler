a6dad3c42b5f761e251c0e79417f6d61a24975e2
Trevor Redding <tareddin@ncsu.edu>
2017-12-19 17:09:28 -0500

    Created a GUI for Scrambler.
    Working on #3. Moved the code for scrambler to a new file, and modified
    scramble and unscramble methods to use file names as parameters. Added
    appropriate methods to help with this. Deleted the previous main method
    since a GUI will be used from now on. Still need to improve GUI, but
    it is functional now.

---
+++ scrambler/ScramblerGUI.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace Scrambler
+{
+    partial class ScramblerGUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_encrypt = new System.Windows.Forms.Button();
+            this.button_decrypt = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
+            this.button_input = new System.Windows.Forms.Button();
+            this.in_file_textbox = new System.Windows.Forms.TextBox();
+            this.label_error = new System.Windows.Forms.Label();
+            this.textBox_key = new System.Windows.Forms.TextBox();
+            this.out_file_textBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button_encrypt
+            // 
+            this.button_encrypt.Location = new System.Drawing.Point(209, 365);
+            this.button_encrypt.Name = "button_encrypt";
+            this.button_encrypt.Size = new System.Drawing.Size(75, 35);
+            this.button_encrypt.TabIndex = 0;
+            this.button_encrypt.Text = "Scramble";
+            this.button_encrypt.UseVisualStyleBackColor = true;
+            this.button_encrypt.Click += new System.EventHandler(this.button_encrypt_Click);
+            // 
+            // button_decrypt
+            // 
+            this.button_decrypt.Location = new System.Drawing.Point(316, 365);
+            this.button_decrypt.Name = "button_decrypt";
+            this.button_decrypt.Size = new System.Drawing.Size(93, 35);
+            this.button_decrypt.TabIndex = 1;
+            this.button_decrypt.Text = "Unscramble";
+            this.button_decrypt.UseVisualStyleBackColor = true;
+            this.button_decrypt.Click += new System.EventHandler(this.button_decrypt_Click);
+            // 
+            // button_input
+            // 
+            this.button_input.Location = new System.Drawing.Point(436, 50);
+            this.button_input.Name = "button_input";
+            this.button_input.Size = new System.Drawing.Size(75, 23);
+            this.button_input.TabIndex = 2;
+            this.button_input.Text = "browse";
+            this.button_input.UseVisualStyleBackColor = true;
+            this.button_input.Click += new System.EventHandler(this.button_input_Click);
+            // 
+            // in_file_textbox
+            // 
+            this.in_file_textbox.Location = new System.Drawing.Point(93, 51);
+            this.in_file_textbox.Name = "in_file_textbox";
+            this.in_file_textbox.Size = new System.Drawing.Size(337, 22);
+            this.in_file_textbox.TabIndex = 3;
+            // 
+            // label_error
+            // 
+            this.label_error.AutoSize = true;
+            this.label_error.Location = new System.Drawing.Point(247, 415);
+            this.label_error.Name = "label_error";
+            this.label_error.Size = new System.Drawing.Size(0, 17);
+            this.label_error.TabIndex = 4;
+            // 
+            // textBox_key
+            // 
+            this.textBox_key.Location = new System.Drawing.Point(93, 131);
+            this.textBox_key.Name = "textBox_key";
+            this.textBox_key.Size = new System.Drawing.Size(337, 22);
+            this.textBox_key.TabIndex = 5;
+            // 
+            // out_file_textBox
+            // 
+            this.out_file_textBox.Location = new System.Drawing.Point(93, 91);
+            this.out_file_textBox.Name = "out_file_textBox";
+            this.out_file_textBox.Size = new System.Drawing.Size(337, 22);
+            this.out_file_textBox.TabIndex = 6;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 17);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Input File:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 17);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Output File:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(51, 136);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 17);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Key:";
+            // 
+            // ScramblerGUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(602, 444);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.out_file_textBox);
+            this.Controls.Add(this.textBox_key);
+            this.Controls.Add(this.label_error);
+            this.Controls.Add(this.in_file_textbox);
+            this.Controls.Add(this.button_input);
+            this.Controls.Add(this.button_decrypt);
+            this.Controls.Add(this.button_encrypt);
+            this.Name = "ScramblerGUI";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button_encrypt;
+        private System.Windows.Forms.Button button_decrypt;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog2;
+        private System.Windows.Forms.Button button_input;
+        private System.Windows.Forms.TextBox in_file_textbox;
+        private System.Windows.Forms.Label label_error;
+        private System.Windows.Forms.TextBox textBox_key;
+        private System.Windows.Forms.TextBox out_file_textBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ scrambler/ScramblerGUI.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Scrambler
+{
+    public partial class ScramblerGUI : Form
+    {
+        String inputFile;
+        String outputFile;
+        public ScramblerGUI()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button_encrypt_Click(object sender, EventArgs e)
+        {
+            label_error.Text = "";
+            try
+            {
+                Scrambler.scramble(in_file_textbox.Text, out_file_textBox.Text, textBox_key.Text);
+                label_error.Text = "File scrambled successfully.";
+            }
+            catch (Exception ex) {
+                label_error.Text = "Error scrambling file: " + ex.Message;
+            }
+        }
+
+        private void button_decrypt_Click(object sender, EventArgs e)
+        {
+            label_error.Text = "";
+            try
+            {
+                Scrambler.unscramble(in_file_textbox.Text, out_file_textBox.Text, textBox_key.Text);
+                label_error.Text = "File unscrambled successfully.";
+            }
+            catch (Exception ex)
+            {
+                label_error.Text = "Error unscrambling file: " + ex.Message;
+            }
+        }
+
+
+        private void button_input_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog file = new OpenFileDialog();
+            if (file.ShowDialog() == DialogResult.OK)
+            {
+                in_file_textbox.Text = file.FileName;
+            }
+        }
+
+
+    }
+}
